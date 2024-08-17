fe4153f4fb9cfecb583492edb674d321da47e01d
Daniels Ciemiņš <ciemins.daniels@gmail.com>
2024-08-17 16:27:27 +0300

    Add basic cv CRUD operations and tests

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,8 +12,11 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-nav-link :href="route('cvs.index')" :active="request()->routeIs('dashboard')">
+                        {{ __('View your CVs') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('cvs.create')" :active="request()->routeIs('dashboard')">
+                        {{ __('Create a new CV') }}
                     </x-nav-link>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CvController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/cvs', [CvController::class, 'index'])->name('cvs.index');
+    Route::get('/cvs/create', [CvController::class, 'create'])->name('cvs.create');
+    Route::post('/cvs', [CvController::class, 'store'])->name('cvs.store');
+    Route::delete('/cvs/{cv}', [CvController::class, 'destroy'])->name('cvs.destroy');
+    Route::get('/cvs/{cv}/edit', [CvController::class, 'edit'])->name('cvs.edit');
+    Route::patch('/cvs/{cv}', [CvController::class, 'update'])->name('cvs.update');
+    Route::get('/cvs/{cv}', [CvController::class, 'show'])->name('cvs.show');
 });
 
 require __DIR__.'/auth.php';
