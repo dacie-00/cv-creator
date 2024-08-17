4782b45af9593312a812172f7ab941a2643b8d1f
Daniels Ciemiņš <ciemins.daniels@gmail.com>
2024-08-17 23:25:15 +0300

    Add user-cv relationship

--- app/Models/Cv.php
+++ app/Models/Cv.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Cv extends Model
@@ -17,4 +19,10 @@ class Cv extends Model
         'phone_number',
         'address',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
