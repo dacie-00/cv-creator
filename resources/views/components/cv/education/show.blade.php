e819dc46b56df80907a7d901807c46f9fc68e9dc
Daniels Ciemiņš <ciemins.daniels@gmail.com>
2024-08-21 19:26:30 +0300

    Update date display

--- resources/views/components/cv/education/show.blade.php
+++ resources/views/components/cv/education/show.blade.php
@@ -7,5 +7,12 @@
         {{ $education->school }}
         <span class="text-sm opacity-50 italic">{{ $education->level }} Education</span>
     </p>
-    <p class="text-sm opacity-50 italic">{{ $education->start_date }} - {{ $education->end_date }}</p>
+    <p class="text-sm opacity-50 italic">
+        @if($education->start_date)
+            {{Carbon::parse($education->start_date)->format('d/m/Y') }}
+        @endif
+        @if($education->end_date)
+            - {{Carbon::parse($education->end_date)->format('d/m/Y') }}
+        @endif
+    </p>
 </li>
--- resources/views/components/cv/work-experience/show.blade.php
+++ resources/views/components/cv/work-experience/show.blade.php
@@ -1,3 +1,4 @@
+@php use Carbon\Carbon; @endphp
 @props(['experience' => null])
 
 <li class="mb-6">
@@ -5,5 +6,12 @@
     <p>
         <span class="text-sm opacity-75">{{ $experience->description }}</span>
     </p>
-    <p class="text-sm opacity-50 italic">{{ $experience->start_date }} - {{ $experience->end_date }}</p>
+    <p class="text-sm opacity-50 italic">
+        @if($experience->start_date)
+            {{Carbon::parse($experience->start_date)->format('d/m/Y') }}
+        @endif
+        @if($experience->end_date)
+            - {{Carbon::parse($experience->end_date)->format('d/m/Y') }}
+        @endif
+    </p>
 </li>
