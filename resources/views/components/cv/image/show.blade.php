f2caebe77b29c9839fbd8cf887acc3a2d2fe2513
Daniels Ciemiņš <ciemins.daniels@gmail.com>
2024-08-21 19:16:16 +0300

    Set CV image size to half

--- resources/views/components/cv/image/show.blade.php
+++ resources/views/components/cv/image/show.blade.php
@@ -1,6 +1,6 @@
 @props(['cv'])
 
-<div>
+<div class="w-1/2">
     @if ($cv->image)
         <img alt="CV profile image" src="{{ asset('storage/' . $cv->id . '.png') }}"
              class="rounded-full aspect-square size-fit object-cover"
